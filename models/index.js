909cc58ece8a54df8d85c5e9bcf026b33bf3c81b
DJ Viking <anders.swedishviking@gmail.com>
2020-08-27 13:56:04 -0400

    set up the foreign keys

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,
+  {
+    foreignKey: 'category_name'
+  }  
+);
 
 // Categories have many Products
+Category.hasMany(Product, 
+  {
+    foreignKey: 'product_name'
+  }
+ );
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,
+  {
+    through: ProductTag,
+    foreignKey: 'tag_name'
+  }
+ );
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,
+  {
+    through: ProductTag,
+    foreignKey: 'product_name'
+  }  
+);
 
 module.exports = {
   Product,
